bde5a30c30a12371f469628b11843e8771254332
Kabir <kabir.batra2003@gmail.com>
2019-06-25 14:14:40 -0700

    runspeed test
    changed run speed to 80 (?) (for testing purposes)

---
+++ IDK/Library/Collab/Base/Assets/NextLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour {
+	void OnTriggerEnter2D() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	void Update() {
+		if (Input.GetKey("r")) {
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+		else if (Input.GetKey("1")) {
+			SceneManager.LoadScene(0);
+		}
+		else if (Input.GetKey("2")) {
+			SceneManager.LoadScene(1);
+		}
+	}
+}
